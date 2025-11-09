e777430b44071ced4387d8d112db4d2af8c293d6
mrtnMorningstar <mivannovv@gmail.com>
2025-11-09 15:31:42 +0200

    Update prices.ts

--- src/lib/prices.ts
+++ src/lib/prices.ts
@@ -1,6 +1,6 @@
 export const PRICE_IDS = {
-  Basic: "price_BasicTestId",
-  Pro: "price_ProTestId",
-  Elite: "price_EliteTestId",
+  Basic: "price_1SRV5LC9GeeSL2vBXBlL7sUP",
+  Pro: "price_1SRV5qC9GeeSL2vBM49Fqb4H",
+  Elite: "price_1SRV6GC9GeeSL2vBLaRFkKKf",
 } as const;
 
