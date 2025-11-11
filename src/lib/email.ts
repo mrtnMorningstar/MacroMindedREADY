b6d79cc1534e7e5b413e9c526951ea8cf0b176a6
mrtnMorningstar <mivannovv@gmail.com>
2025-11-12 01:39:20 +0200

    Update email.ts

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -2,7 +2,13 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendEmail({ to, subject, html }) {
+type SendEmailArgs = {
+  to: string;
+  subject: string;
+  html: string;
+};
+
+export async function sendEmail({ to, subject, html }: SendEmailArgs) {
   try {
     await resend.emails.send({
       from: process.env.EMAIL_FROM, // must be support@macrominded.net
